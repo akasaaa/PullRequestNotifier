b50c0b286c5b5a38e7c6cb1aaf36941b37bebad7
赤迫亮太 <>
2024-08-24 07:49:17 +0900

    Add Files

---
+++ PullRequestNotifier/PullRequestNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullRequestNotifierApp.swift
+//  PullRequestNotifier
+//
+//  Created by 赤迫亮太 on 2024/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PullRequestNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
